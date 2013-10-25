260ba9de5f1f3b0e2963eabfbde0809d272807a7
Andreas Marc Klingler <post@andreas-klingler.de>
2013-10-25 11:52:20 +0200

    MVC for calendar created.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Tam::Application.routes.draw do
+  resources :calendars
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
