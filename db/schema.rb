f3cc0d2ea2c90ff670f3fbd966c1b68e8b4e2689
Andreas Marc Klingler <post@andreas-klingler.de>
2013-10-25 11:55:23 +0200

    MVC for activity created.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131025095029) do
+ActiveRecord::Schema.define(version: 20131025095241) do
+
+  create_table "activities", force: true do |t|
+    t.string   "name"
+    t.string   "tag"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "calendars", force: true do |t|
     t.string   "name"
