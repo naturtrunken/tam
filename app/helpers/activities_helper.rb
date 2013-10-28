2613d63893a322e7b029f259c1c61667cd49a4f3
Andreas Marc Klingler <post@andreas-klingler.de>
2013-10-28 12:07:56 +0100

    Tag for an activity inserted in the index page of the activities.

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,2 +1,36 @@
 module ActivitiesHelper
+
+  # Given:
+  #  [0] An activity object
+  #
+  # Returns:
+  #  A string with the unique identifier for the given activity.
+  #
+  def activity_tag_for(activity)
+
+    return '!' unless activity
+    current_activity = activity
+    ret_id = ''
+
+    # Iterate through the whole tree to the top.
+    while (current_activity)
+
+      # Prepend the if of the current activity to the return string.
+      ret_id = current_activity.id.to_s + '-' + ret_id
+
+      # Go one stage higher, if there is another parent node.
+      if current_activity.parent
+        # But first make sure that the parent node still exists.
+        return '#!' + ret_id unless Activity.where(:id => current_activity.parent).count > 0
+        current_activity = Activity.find(current_activity.parent)
+      else
+        current_activity = nil
+      end
+
+    end
+
+    # Return the created string without the trailing '-'.
+    '#' + ret_id[0..ret_id.length-2]
+  end
+
 end
