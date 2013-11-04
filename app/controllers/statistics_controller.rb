c086cd289e6096377b53ec1753df985358b6ded8
Andreas Marc Klingler <post@andreas-klingler.de>
2013-11-04 13:37:19 +0100

    Year and calendar week selection inserted.

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -4,7 +4,32 @@ class StatisticsController < ApplicationController
   include ActivitiesHelper
 
   def index
-    @report = report_for_week(2013, 44)
+
+    @available_weeks = []
+    0.upto(9) do |i|
+      @available_weeks.push((Date.today - i.week).cweek)
+    end
+
+    if params[:week]
+      # TODO input validation
+      @current_week = params[:week].to_i
+    else
+      @current_week = Date.today.cweek
+    end
+
+    @available_years = []
+    0.upto(3) do |i|
+      @available_years.push((Date.today - i.year).year)
+    end
+
+    if params[:year]
+      # TODO input validation
+      @current_year = params[:year].to_i
+    else
+      @current_year = Date.today.year
+    end
+
+    @report = report_for_week(@current_year, @current_week)
     @statistic_for_days = statistic_for_report(@report)
   end
 
